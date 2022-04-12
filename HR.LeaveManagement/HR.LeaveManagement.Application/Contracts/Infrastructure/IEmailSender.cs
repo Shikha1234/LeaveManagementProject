c677192e1191d57d4f8effc4caa348e7020736a1
SPandey <shikhapandey95@gmail.com>
2022-04-12 13:46:02 +0530

    Added Email Class , Models Folder and Infrastucuture folder inside contracts

---
+++ HR.LeaveManagement/HR.LeaveManagement.Application/Contracts/Infrastructure/IEmailSender.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Contracts.Infrastructure
+{
+    public class IEmailSender
+    {
+        Task<bool> SendEmail(Email email);
+    }
+}
